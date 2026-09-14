5436a75c5a3ad550b10227eb9beb2c29b209cf8f
MasElonn <riendraabi009@gmail.com>
2026-09-14 18:43:24 +0700

    fix vercel

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ try {
   Groq = null;
 }
 
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "NusaQuest Server is running!" });
+});
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
